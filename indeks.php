d9a7050d7ab9ff73719a24e4156be06edb1eb51f
azel <vincentius752@gmail.com>
2026-01-14 13:48:26 +0700

    merubah html

--- indeks.php
+++ indeks.php
@@ -8,6 +8,10 @@
 <body>
     <h1>Halo Dunia!</h1>
     <p>Ini adalah halaman uji coba.</p>
-    
+    <ul>
+        <li><a href="form.html">Form HTML</a></li>
+        <li><a href="proses.php">Proses PHP</a></li>
+        <li><a href="data.json">Data JSON</a></li>
+    </ul>
 </body>
 </html>
